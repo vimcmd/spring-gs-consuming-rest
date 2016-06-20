66739fd3a15f0dd9a504f2bb6dcdbaf8fc549399
vim.cmd <vim.cmd@gmail.com>
2016-06-20 11:00:52 +0500

    making the application executable

---
+++ src/main/java/hello/Application.java
@@ -0,0 +1,31 @@
+package hello;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * Because the Jackson JSON processing library is in the classpath,
+ * RestTemplate will use it (via a message converter) to convert the incoming JSON
+ * data into a Quote object. From there, the contents of the Quote object
+ * will be logged to the console.
+ */
+@SpringBootApplication
+public class Application implements CommandLineRunner {
+
+    private static final Logger log = LoggerFactory.getLogger(Application.class);
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        RestTemplate restTemplate = new RestTemplate();
+        Quote quote = restTemplate.getForObject("http://gturnquist-quoters.cfapps.io/api/random", Quote.class);
+        log.info(quote.toString());
+    }
+}
